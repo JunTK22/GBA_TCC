f02ba3fc2590a327a2ff7765ba5f7608d9f188e3
caio <caiojnu22@usp.br>
2026-07-03 21:45:39 -0300

    bg_rendered small advancements

--- ppu/bg_renderer.v
+++ ppu/bg_renderer.v
@@ -45,15 +45,16 @@ module moduleName (
     input wire  [15:0]  BG3_PD
 );
 
-    localparam IDLE         = 0;
-    localparam SETUP        = 0;
-    localparam INIT_FETCH   = 0;
-    localparam BG1_RENDER   = 0;
-    localparam BG2_RENDER   = 0;
-    localparam BG3_RENDER   = 0;
-    localparam BG4_RENDER   = 0;
+    localparam START        = 0;
+    localparam IDLE         = 1;
+    localparam SETUP        = 2;
+    localparam INIT_FETCH   = 3;
+    localparam BG1_RENDER   = 4;
+    localparam BG2_RENDER   = 5;
+    localparam BG3_RENDER   = 6;
+    localparam BG4_RENDER   = 7;
 
-    reg [9:0] STATE, NEXT_STATE = 0;
+    reg [9:0] STATE, NEXT_STATE = START;
     
     wire BG0_8bpp = BG0_CTN[7];
     wire BG1_8bpp = BG1_CTN[7];
@@ -70,8 +71,46 @@ module moduleName (
     reg [15:0]  BG2_pixel = 16'b0;
     reg [15:0]  BG3_pixel = 16'b0;
 
+    always @(posedge clock or negedge nrst) begin
+        if (!nrst) STATE <= START;
+        else STATE <= NEXT_STATE;
+    end
 
+    always @(*) begin
+        case (STATE)
+            START: begin
+                BG0_map_entry <= 16'b0;
+                BG1_map_entry <= 16'b0;
+                BG2_map_entry <= 16'b0;
+                BG3_map_entry <= 16'b0;
 
+                BG0_pixel <= 16'b0;
+                BG1_pixel <= 16'b0;
+                BG2_pixel <= 16'b0;
+                BG3_pixel <= 16'b0;
 
+                NEXT_STATE <= START;
+            end
+            IDLE: begin
+                BG0_map_entry <= 16'b0;
+                BG1_map_entry <= 16'b0;
+                BG2_map_entry <= 16'b0;
+                BG3_map_entry <= 16'b0;
+
+                BG0_pixel <= 16'b0;
+                BG1_pixel <= 16'b0;
+                BG2_pixel <= 16'b0;
+                BG3_pixel <= 16'b0;
+
+                NEXT_STATE <= SETUP;
+            end
+            SETUP: begin
+                bg_addr <= BG0_CTN[12:8] * 1024
+
+                NEXT_STATE <= SETUP;
+            end
+            default: 
+        endcase
+    end
 
 endmodule
